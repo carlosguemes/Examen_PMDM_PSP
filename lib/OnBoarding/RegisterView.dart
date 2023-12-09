816980f52ba36709b8a31bfce06fbec860e3e247
carlos <carlosguemesdam@gmail.com>
2023-12-09 12:32:18 +0100

    PSP 1,2: Administrar registro en FirebaseAdmin y botones onPressed

--- lib/OnBoarding/RegisterView.dart
+++ lib/OnBoarding/RegisterView.dart
@@ -1,3 +1,4 @@
+import 'package:examen_carlos_guemes/Singletone/DataHolder.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -16,30 +17,12 @@ class RegisterView extends StatelessWidget{
   );
 
   void onClickAceptarRegistrar() async {
-    if (passwordController.text == repasswordController.text) {
-      try {
-        await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: usernameController.text,
-          password: passwordController.text,
-        );
-        Navigator.of(_context).popAndPushNamed('/homeview');
-      } on FirebaseAuthException catch (e) {
-        if (e.code == 'weak-password') {
-          print('The password provided is too weak.');
-        } else if (e.code == 'email-already-in-use') {
-          print('The account already exists for that email.');
-        }
-      } catch (e) {
-        print(e);
-      }
-    }
-    else{
-      ScaffoldMessenger.of(_context).showSnackBar(snackBar);
-    }
+    DataHolder().fa.onClickRegistrar
+      (usernameController, passwordController, repasswordController);
   }
 
   void onClickCancelarRegistrar(){
-    Navigator.of(_context).popAndPushNamed('/loginview');
+    //Navigator.of(_context).popAndPushNamed('/loginview');
   }
 
   @override
--- lib/Singletone/FirebaseAdmin.dart
+++ lib/Singletone/FirebaseAdmin.dart
@@ -1,3 +1,29 @@
-class FirebaseAdmin{
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
+class FirebaseAdmin{
+  void onClickRegistrar (TextEditingController usernameController,
+      TextEditingController passwordController,
+      TextEditingController repasswordController) async{
+    if (passwordController.text == repasswordController.text) {
+      try {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: usernameController.text,
+          password: passwordController.text,
+        );
+        //Navigator.of(_context).popAndPushNamed('/homeview');
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'weak-password') {
+          print('The password provided is too weak.');
+        } else if (e.code == 'email-already-in-use') {
+          print('The account already exists for that email.');
+        }
+      } catch (e) {
+        print(e);
+      }
+    }
+    else{
+      print('Las contraseñas tienen que ser iguales');
+    }
+  }
 }
